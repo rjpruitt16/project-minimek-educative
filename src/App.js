b273709235d8a68bde6942c9fbe92c840a910a94
Rahmi Pruitt <rjpruitt16@gmail.com>
2018-09-12 17:38:46 -0500

    Something

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {Header} from "semantic-ui-react";
+
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <Header inverted as="h1">Project Mini-Mek</Header>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
